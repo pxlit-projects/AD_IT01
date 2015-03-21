e3ecb148bb16efda537435c48e0d83d663596e50
RubenCox <cox.ruben@hotmail.com>
2015-03-21 22:46:32 +0100

    HTML & CSS validator check
    De html code en de css code is gecontroleerd volgens de regels van W3C (
    1ste maal )

--- finah-web/javascript/javascript.js
+++ finah-web/javascript/javascript.js
@@ -1,4 +1,20 @@
+$(document).ready(function() {
 
+$('body').css('display', 'none');
+
+$('body').fadeIn(2000);
+});
+
+
+
+function playVid() { 
+	var vid = document.getElementById("inleidingVideo");
+    vid.play(); 
+} 
+
+function pauseVid() { 
+    vid.pause(); 
+}
 
 function hideFeedbackDiv() {
     var feedback = document.getElementById("feedback");
@@ -27,6 +43,6 @@ function hideInfoDiv() {
 	$('#startInleiding').fadeOut(900);
 	setTimeout(function(){showVideoIntroDiv()},1000);
 	setTimeout(function(){showStartEnqueteDiv()},1000);
-	
+	setTimeout(function(){playVid()},5000);
 }
 
