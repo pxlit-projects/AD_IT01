de07ac1b94fa9c8c3a46a0e765b5932348d802c4
RubenCox <cox.ruben@hotmail.com>
2015-04-01 09:23:31 +0200

    Restore User Details
    User details

--- finah-web/php/userDetails.php
+++ finah-web/php/userDetails.php
@@ -1,19 +1,34 @@
-        <div class="col-md-offset-2 col-md-8">
-            <legend>Welkom</legend>
-            <?php
-                $database = new medoo();
-                $user = $database->select(
-                    "users", "*", [
-                    "AND" => [
-                    "username" => $_SESSION['username'],
-                    "password" => $_SESSION['password']
-                    ]]);
-                echo '<p>Welkom ' . $user[0]['firstName'] . " " . $user[0]['lastName'] . ".</p>";
-            ?>
-            <p>FINAH is een digitaal meetinstrument waarmee het functioneren van personen met NAH op vlak van activiteiten en participatie op efficiënte wijze in kaart kan worden gebracht. Dit gebeurt door de ervaren hinder te bevragen bij zowel de persoon met NAH en zijn/haar mantelzorger.</p>
-            <p>Op basis van de antwoorden wordt een overzichtsrapport gegenereerd. Deze werkwijze zorgt ervoor dat op tijdsefficiënte wijze een globaal beeld wordt gevormd, op basis waarvan de hulpverlener het gesprek kan aangaan en een adequaat behandelplan kan opstellen.</p>
+        
+	<header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-text">
+                        <span class="name">Welkom</span>
+                        <hr class="star-light">
+                        <span class="skills">
+						<?php
+								$database = new medoo();
+								$user = $database->select(
+									"users", "*", [
+									"AND" => [
+									"username" => $_SESSION['username'],
+									"password" => $_SESSION['password']
+									]]);
+								echo '<p>' . $user[0]['firstName'] . " " . $user[0]['lastName'] . ".</p>";
+							?>
+						</span>
+                    </div>
+                </div>
+            </div>
+			<div class="row">
+                <div class="col-lg-12">
+					<p>FINAH is een digitaal meetinstrument waarmee het functioneren van personen met NAH op vlak van activiteiten en participatie op efficiënte wijze in kaart kan worden gebracht. Dit gebeurt door de ervaren hinder te bevragen bij zowel de persoon met NAH en zijn/haar mantelzorger.</p>
+					<p>Op basis van de antwoorden wordt een overzichtsrapport gegenereerd. Deze werkwijze zorgt ervoor dat op tijdsefficiënte wijze een globaal beeld wordt gevormd, op basis waarvan de hulpverlener het gesprek kan aangaan en een adequaat behandelplan kan opstellen.</p>
+                </div>
+            </div>
         </div>
-    </div>
+    </header>
     <?php
         if($user[0]['rank'] > 9)
         {
@@ -21,4 +36,3 @@
         }
         include 'html/userInfo.html';
     ?>
-</div>
