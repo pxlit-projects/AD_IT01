26c3e784b4bf1a1fde2b3fe9165157c1dc22f89d
RubenCox <cox.ruben@hotmail.com>
2015-05-05 18:48:20 +0200

    Desktop Applicatie 2
    Verder werken desktop applicatie

---
+++ finah-desktop/Visual Studio Projects/ModernUINavigationApp1/ModernUINavigationApp1/Pages/Patienten/Patient1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernUINavigationApp1.Pages.Patienten
+{
+    /// <summary>
+    /// Interaction logic for Patient1.xaml
+    /// </summary>
+    public partial class Patient1 : UserControl
+    {
+        public Patient1()
+        {
+            InitializeComponent();
+        }
+    }
+}
