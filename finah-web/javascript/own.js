fbd86c4754739963b9fa35551a89bfec7e63d099
RubenCox <cox.ruben@hotmail.com>
2015-05-12 11:03:54 +0200

    Werken aan Desktop en Web

--- finah-web/javascript/own.js
+++ finah-web/javascript/own.js
@@ -8,5 +8,23 @@ $( document ).ready(function() {
 	$( "#GegWijzigen" ).click(function() {
 	$( "#GgAanpassen").toggle();
 });
+
+$( "#OPatient" ).click(function() {
+	$( "#OverzPat").toggle();
+});
+$( "#NPatient" ).click(function() {
+	$( "#NieuwPat").toggle();
+});
+
+$( "#LAanvraag" ).click(function() {
+	$( "#textA").text("Lopende");
+});
+$( "#OAanvraag" ).click(function() {
+	$( "#textA").text("Ongeopend");
+});
+$( "#NAanvraag" ).click(function() {
+	$( "#textA").text("Nieuwe Aanvraag");
+});
+
  
 });
