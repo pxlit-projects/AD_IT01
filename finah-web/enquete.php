6878ad92ae738fd4879c4d6f37ff3fc084836fcd
Kevin Coenegrachts <kevin_real3@hotmail.com>
2015-05-22 15:08:36 +0200

    Survey is complete and answered are saved

--- finah-web/enquete.php
+++ finah-web/enquete.php
@@ -52,6 +52,21 @@
                     "authkey" => $_GET['authkey']
                     ]]);
 
+            if(isset($_GET['beantwoord'], $_GET['radio']))
+            {
+            	if(!$database->has("antwoorden", ["AND" => ["vraagid" => $_GET['beantwoord'], "userid" => $_GET['surveyuser']]]))
+            	{
+            		if(isset($_GET['radio2']))
+            		{
+            			$database->insert("antwoorden", ["vraagid" => $_GET['beantwoord'], "userid" => $_GET['surveyuser'], "antwoord" => $_GET['radio'], "werk" => $_GET['radio2']]);
+            		}
+            		else
+            		{
+            			$database->insert("antwoorden", ["vraagid" => $_GET['beantwoord'], "userid" => $_GET['surveyuser'], "antwoord" => $_GET['radio'], "werk" => 0]);
+            		}
+            	}
+            }
+
             if(!isset($_GET['vraagid']))
             {
                 $lastQuestion = $database->count(
@@ -73,121 +88,190 @@
                     $amountofquestions = $database->count("vragen", "*");
 
                     $question = $database->get("vragen", "*", [
-                    "vraagid" => $lastQuestion
+                    "vraagid" => ($lastQuestion + 1)
                     ]);
 
-                    $theme = $database->get("thema", "*", [
-                    "themaid" => $question['themaid']
-                    ]);
+                    if($lastQuestion == $amountofquestions)
+                    {
+                    	echo '<div class="row" id="rowTopMargin50px">';
+	                    echo '<div class="col-md-offset-2 col-md-8">';
+		                	echo '<p>De vragenlijst is volledig ingevuld.</p>';
+		                	echo '<form action="index.php">';
+		                	echo '<div class="col-lg-2 col-md-offset-8">';
+		                        echo '<button type="submit" class="btn btn-default btn-lg btn-block">Afsluiten</button>';
+			                    echo '</div>';
+		                    echo '</div>';
+	                    echo '</div>';
+                    }
+                    else
+                    {
+	                    $theme = $database->get("thema", "*", [
+	                    "themaid" => $question['themaid']
+	                    ]);
 
-                    echo '<div class="row" id="rowTopMargin50px">';
-                        echo '<div class="col-md-offset-2 col-md-8">';
-                            echo '<legend>' . $theme['themanaam'] . '</legend>';
-                            echo '<p>' . $question['vraag'] . '</p>';
-                            echo '<img src="' . $question['logo'] . '" alt="" class="img-responsive center-block" />';
-                            ?>
-                        <div id="labelDiv">
-                            <input type="radio" name="radio" id="radio1" class="radio"/>
-                            <label id="radioLabel" for="radio1">Verloopt naar wens</label>
-                        </div>
+	                    echo '<div class="row" id="rowTopMargin50px">';
+	                        echo '<div class="col-md-offset-2 col-md-8">';
+	                            echo '<legend>' . $theme['themanaam'] . '</legend>';
+	                            echo '<p>' . $question['vraag'] . '</p>';
+	                            echo '<img src="' . $question['logo'] . '" alt="" class="img-responsive center-block" />';
+	                            ?>
+	                        <div class="row">
+		                            <form method="get" action="enquete.php">
+			                            <?php
+						                	echo '<input type="text" class="form-control" id="vraagid" name="surveyuser" value="' . $_GET['surveyuser'] . '">';
+						                	echo '<input type="text" class="form-control" id="vraagid" name="authkey" value="' . $_GET['authkey'] . '">';
+						                	echo '<input type="text" class="form-control" id="vraagid" name="vraagid" value="' . ($lastQuestion + 1) . '">';
+						                	echo '<input type="text" class="form-control" id="vraagid" name="beantwoord" value="' . $lastQuestion . '">';
+						                ?>
+		                                <div class="col-md-6">
+		                                    <div id="labelDiv">
+		                                        <input type="radio" name="radio" id="radio1" class="radio" value="1" onclick="showNextButtonWithoutFeedback()"/>
+		                                        <label id="radioLabel" for="radio1">Verloopt naar wens</label>
+		                                    </div>
 
-                        <div id="labelDiv">
-                            <input type="radio" name="radio" id="radio2" class="radio"/>
-                            <label id="radioLabel" for="radio2">Probleem - niet hinderlijk</label>
-                        </div>
+		                                    <div id="labelDiv">
+		                                        <input type="radio" name="radio" id="radio2" class="radio" value="2" onclick="showNextButtonWithoutFeedback()"/>
+		                                        <label id="radioLabel" for="radio2">Probleem - niet hinderlijk</label>
+		                                    </div>
 
-                        <div id="labelDiv">   
-                            <input type="radio" name="radio" id="radio3" class="radio"/>
-                            <label id="radioLabel" for="radio3">Probleem - hinderlijk voor cliënt</label>
-                        </div>
+		                                    <div id="labelDiv">   
+		                                        <input type="radio" name="radio" id="radio3" class="radio" value="3" onclick="showFeedbackDiv()"/>
+		                                        <label id="radioLabel" for="radio3">Probleem - hinderlijk voor cliënt</label>
+		                                    </div>
 
-                        <div id="labelDiv">   
-                            <input type="radio" name="radio" id="radio4" class="radio"/>
-                            <label id="radioLabel" for="radio4">Probleem - hinderlijk voor mantelzorger</label>
-                        </div>
+		                                    <div id="labelDiv">   
+		                                        <input type="radio" name="radio" id="radio4" class="radio" value="4" onclick="showFeedbackDiv()"/>
+		                                        <label id="radioLabel" for="radio4">Probleem - hinderlijk voor mantelzorger</label>
+		                                    </div>
 
-                        <div id="labelDiv">   
-                            <input type="radio" name="radio" id="radio5" class="radio"/>
-                            <label id="radioLabel" for="radio5">Probleem - hinderlijk voor beide</label>
-                        </div>
-                        <?php
-                        echo '</div>';
-                    echo '</div>';
-                }
+		                                    <div id="labelDiv">   
+		                                        <input type="radio" name="radio" id="radio5" class="radio" value="5" onclick="showFeedbackDiv()"/>
+		                                        <label id="radioLabel" for="radio5">Probleem - hinderlijk voor beide</label>
+		                                    </div>
+		                                </div>
+		                                <div class="col-md-6" id="feedback">
+		                                    <p>Wilt u dat we hieraan werken?</p>
+
+		                                    <div id="labelDiv">
+		                                        <input type="radio" name="radio2" id="radio6" class="radio" value="1" onclick="showNextButton()" />
+		                                        <label id="radioLabel" for="radio6">Ja</label>
+		                                    </div>
+
+		                                    <div id="labelDiv">
+		                                        <input type="radio" name="radio2" id="radio7" class="radio" value="0" onclick="showNextButton()" />
+		                                        <label id="radioLabel" for="radio7">Neen</label>
+		                                    </div>
+		                                </div>
+		                                <div class="col-lg-12" style="margin-top: 40px;">
+		                                	<div class="progress">
+											  <?php echo '<div class="progress-bar progress-bar-info" style="width:' . ($lastQuestion * 100 / $amountofquestions) . '%"></div>'; ?>
+											</div>
+		                                </div>
+		                                <div class="col-lg-2 col-md-offset-8" id="nextButton">
+		                                    <button type="submit" class="btn btn-default btn-lg btn-block">Volgende</button>
+		                                </div>
+		                            </form>
+		                        </div>
+	                        <?php
+	                        echo '</div>';
+	                    echo '</div>';
+	                }
+	            }
             }
             else
             {
                 $amountofquestions = $database->count("vragen", "*");
 
-                $question = $database->get("vragen", "*", [
-                    "vraagid" => $_GET['vraagid']
-                    ]);
-
-                $theme = $database->get("thema", "*", [
-                    "themaid" => $question['themaid']
-                    ]);
-
-                echo '<div class="row" id="rowTopMargin50px">';
-                    echo '<div class="col-md-offset-2 col-md-8">';
-                        echo '<legend>' . $theme['themanaam'] . '</legend>';
-                        echo '<img src="' . $question['logo'] . '" alt="" class="img-responsive center-block" />';
-                        echo '<p>' . $question['vraag'] . '</p>';
-                        ?>
-                        <div class="row">
-                            <form>
-                                <div class="col-md-6">
-                                    <div id="labelDiv">
-                                        <input type="radio" name="radio" id="radio1" class="radio" value="1" onclick="showNextButtonWithoutFeedback()"/>
-                                        <label id="radioLabel" for="radio1">Verloopt naar wens</label>
-                                    </div>
-
-                                    <div id="labelDiv">
-                                        <input type="radio" name="radio" id="radio2" class="radio" value="2" onclick="showNextButtonWithoutFeedback()"/>
-                                        <label id="radioLabel" for="radio2">Probleem - niet hinderlijk</label>
-                                    </div>
-
-                                    <div id="labelDiv">   
-                                        <input type="radio" name="radio" id="radio3" class="radio" value="3" onclick="showFeedbackDiv()"/>
-                                        <label id="radioLabel" for="radio3">Probleem - hinderlijk voor cliënt</label>
-                                    </div>
-
-                                    <div id="labelDiv">   
-                                        <input type="radio" name="radio" id="radio4" class="radio" value="4" onclick="showFeedbackDiv()"/>
-                                        <label id="radioLabel" for="radio4">Probleem - hinderlijk voor mantelzorger</label>
-                                    </div>
-
-                                    <div id="labelDiv">   
-                                        <input type="radio" name="radio" id="radio5" class="radio" value="5" onclick="showFeedbackDiv()"/>
-                                        <label id="radioLabel" for="radio5">Probleem - hinderlijk voor beide</label>
-                                    </div>
-                                </div>
-                                <div class="col-md-6" id="feedback">
-                                    <p>Wilt u dat we hieraan werken?</p>
-
-                                    <div id="labelDiv">
-                                        <input type="radio" name="radio2" id="radio6" class="radio" value="1" onclick="showNextButton()" />
-                                        <label id="radioLabel" for="radio6">Ja</label>
-                                    </div>
-
-                                    <div id="labelDiv">
-                                        <input type="radio" name="radio2" id="radio7" class="radio" value="0" onclick="showNextButton()" />
-                                        <label id="radioLabel" for="radio7">Neen</label>
-                                    </div>
-                                </div>
-                                <div class="col-lg-12" style="margin-top: 40px;">
-                                	<div class="progress">
-									  <?php echo '<div class="progress-bar progress-bar-info" style="width:' . ($_GET['vraagid'] * 100 / $amountofquestions) . '%"></div>'; ?>
-									</div>
-                                </div>
-                                <div class="col-lg-2 col-md-offset-8" id="nextButton">
-                                    <button type="button" class="btn btn-default btn-lg btn-block">Volgende</button>
-                                </div>
-                            </form>
-                        </div>
-                <?php
+                if($_GET['vraagid'] > $amountofquestions)
+                {
+                	echo '<div class="row" id="rowTopMargin50px">';
+	                    echo '<div class="col-md-offset-2 col-md-8">';
+		                	echo '<p>De vragenlijst is volledig ingevuld.</p>';
+		                	echo '<form action="index.php">';
+		                	echo '<div class="col-lg-2 col-md-offset-8">';
+		                        echo '<button type="submit" class="btn btn-default btn-lg btn-block">Afsluiten</button>';
+		                    echo '</div>';
+	                    echo '</div>';
                     echo '</div>';
-                echo '</div>';
-            }
+                }
+                else
+                {
+	                $question = $database->get("vragen", "*", [
+	                    "vraagid" => $_GET['vraagid']
+	                    ]);
+
+	                $theme = $database->get("thema", "*", [
+	                    "themaid" => $question['themaid']
+	                    ]);
+
+	                echo '<div class="row" id="rowTopMargin50px">';
+	                    echo '<div class="col-md-offset-2 col-md-8">';
+	                        echo '<legend>' . $theme['themanaam'] . '</legend>';
+	                        echo '<img src="' . $question['logo'] . '" alt="" class="img-responsive center-block" />';
+	                        echo '<p>' . $question['vraag'] . '</p>';
+	                        ?>
+	                        <div class="row">
+	                            <form method="get" action="enquete.php">
+		                            <?php
+					                	echo '<input type="text" class="form-control" id="vraagid" name="surveyuser" value="' . $_GET['surveyuser'] . '">';
+					                	echo '<input type="text" class="form-control" id="vraagid" name="authkey" value="' . $_GET['authkey'] . '">';
+					                	echo '<input type="text" class="form-control" id="vraagid" name="vraagid" value="' . ($_GET['vraagid'] + 1) . '">';
+					                	echo '<input type="text" class="form-control" id="vraagid" name="beantwoord" value="' . $_GET['vraagid'] . '">';
+					                ?>
+	                                <div class="col-md-6">
+	                                    <div id="labelDiv">
+	                                        <input type="radio" name="radio" id="radio1" class="radio" value="1" onclick="showNextButtonWithoutFeedback()"/>
+	                                        <label id="radioLabel" for="radio1">Verloopt naar wens</label>
+	                                    </div>
+
+	                                    <div id="labelDiv">
+	                                        <input type="radio" name="radio" id="radio2" class="radio" value="2" onclick="showNextButtonWithoutFeedback()"/>
+	                                        <label id="radioLabel" for="radio2">Probleem - niet hinderlijk</label>
+	                                    </div>
+
+	                                    <div id="labelDiv">   
+	                                        <input type="radio" name="radio" id="radio3" class="radio" value="3" onclick="showFeedbackDiv()"/>
+	                                        <label id="radioLabel" for="radio3">Probleem - hinderlijk voor cliënt</label>
+	                                    </div>
+
+	                                    <div id="labelDiv">   
+	                                        <input type="radio" name="radio" id="radio4" class="radio" value="4" onclick="showFeedbackDiv()"/>
+	                                        <label id="radioLabel" for="radio4">Probleem - hinderlijk voor mantelzorger</label>
+	                                    </div>
+
+	                                    <div id="labelDiv">   
+	                                        <input type="radio" name="radio" id="radio5" class="radio" value="5" onclick="showFeedbackDiv()"/>
+	                                        <label id="radioLabel" for="radio5">Probleem - hinderlijk voor beide</label>
+	                                    </div>
+	                                </div>
+	                                <div class="col-md-6" id="feedback">
+	                                    <p>Wilt u dat we hieraan werken?</p>
+
+	                                    <div id="labelDiv">
+	                                        <input type="radio" name="radio2" id="radio6" class="radio" value="1" onclick="showNextButton()" />
+	                                        <label id="radioLabel" for="radio6">Ja</label>
+	                                    </div>
+
+	                                    <div id="labelDiv">
+	                                        <input type="radio" name="radio2" id="radio7" class="radio" value="0" onclick="showNextButton()" />
+	                                        <label id="radioLabel" for="radio7">Neen</label>
+	                                    </div>
+	                                </div>
+	                                <div class="col-lg-12" style="margin-top: 40px;">
+	                                	<div class="progress">
+										  <?php echo '<div class="progress-bar progress-bar-info" style="width:' . ($_GET['vraagid'] * 100 / $amountofquestions) . '%"></div>'; ?>
+										</div>
+	                                </div>
+	                                <div class="col-lg-2 col-md-offset-8" id="nextButton">
+	                                    <button type="submit" class="btn btn-default btn-lg btn-block">Volgende</button>
+	                                </div>
+	                            </form>
+	                        </div>
+	                <?php
+	                    echo '</div>';
+	                echo '</div>';
+	            }
+        	}
         ?>
     </body>
 </html>
