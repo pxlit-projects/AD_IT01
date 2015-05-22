458a523f4deb2721dc2294a8e51006ad28e19925
Kevin Coenegrachts <kevin_real3@hotmail.com>
2015-05-22 18:37:41 +0200

    Rapport gets generated based on answers

--- finah-web/rapport.php
+++ finah-web/rapport.php
@@ -1,67 +1,190 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>FINAH-NAH</title>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="javascript/jquery-1.11.2.min.js"></script>
-        <script src="javascript/javascript.js"></script>
-        <script src="javascript/general.js"></script>
+<?php
+    require_once 'medoo.min.php';
+    $database = new medoo();
+    require('fpdf.php');
 
+    class PDF extends FPDF
+    {
+        function Header()
+        {
+            global $title;
 
-		<script src="javascript/bootstrap.js"></script>
-		<script src="javascript/freelancer.js"></script>
-		<script src="javascript/own.js"></script>
-		<link rel="stylesheet" type="text/css" href="css/freelancer.css">
+            // Arial bold 15
+            $this->SetFont('Arial','B',15);
+            // Move to the right
+            $this->Cell(80);
+            // Title
+            $this->Cell(30,10,$title,0,0,'C');
+            // Line break
+            $this->Ln(20);
+        }
 
-        <script src="javascript/bootstrap.js"></script>
-        <link rel="stylesheet" type="text/css" href="css/customBootstrap.css">
-		<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-		<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-		<link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-		
-		<link rel="icon" type="image/png" href="images/logo/logo.png">
-    </head>
-    <body>
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed page-scroll" data-toggle="collapse" data-target="#navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand page-scroll" href="index.php">FINAH-NAH</a>
-                </div>
+        // Page footer
+        function Footer()
+        {
+            // Position at 1.5 cm from bottom
+            $this->SetY(-15);
+            // Arial italic 8
+            $this->SetFont('Arial','I',8);
+            // Page number
+            $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+        }
+    }
 
-                <div class="collapse navbar-collapse" id="navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="php/logout.php">Uitloggen</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    if(isset($_GET['surveyuser']))
+    {
+        $survey1 = $database->get("surveyusers", "*", ["surveyUserid" => $_GET['surveyuser']]);
+        $userid1 = $survey1['user'];
+        $userid2 = $survey1['linkedUser'];
+        $survey2 = $database->get("surveyusers", "*", ["user" => $userid2]);
 
-        <?php 
-            require_once 'medoo.min.php';
-            $database = new medoo();
-        ?>
+        $user1 = $database->get("users", "*", ["userid" => $userid1]);
+        $user2 = $database->get("users", "*", ["userid" => $userid2]);
 
-        <section class="" id="aanvragen">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 text-center">
-                        <p class="title">Rapport</p>
-                        <hr class="star-primary">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-8 col-lg-offset-2">
-                    </div>
-                </div>
-             </div>
-        </section>
-    </body>
-</html>
+        $title = 'Rapport: ' . $user1['firstName'] . ' ' . $user1['lastName'] . ' - ' . $user2['firstName'] . ' ' . $user2['lastName'];
+        $pdf = new PDF();
+        $pdf->AliasNbPages();
+        $pdf->AddPage();
+        $pdf->SetTitle($title);  
+
+        if($survey2['status'] == 2)
+        {
+            $antwoordenUser1 = $database->select("antwoorden", "*", ["userid" => $_GET['surveyuser']]);
+
+            foreach ($antwoordenUser1 as $antwoordUser1) {
+                $antwoordUser2 = $database->get("antwoorden", "*", ["AND" => ["userid" => $survey2['surveyUserid'], "vraagid" => $antwoordUser1['vraagid']]]);
+
+                if($antwoordUser1['werk'] == 1 || $antwoordUser2['werk'] == 1)
+                {
+                    $vraag = $database->get("vragen", "*", ["vraagid" => $antwoordUser1['vraagid']]);
+                    $pdf->SetFont('Times','U',12);
+                    $pdf->Cell(0,10,$vraag['vraagKort'] . ':',0,1);
+                    $pdf->SetFont('Times', '', 10);
+
+                    if($antwoordUser1['antwoord'] == 3)
+                    {
+                        if($user1['rank'] == 1)
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor client gerapporteerd door client.",0,1);
+                        }
+                        else
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor client gerapporteerd door mantelzorger.",0,1);
+                        }
+                    }
+                    else if($antwoordUser1['antwoord'] == 4)
+                    {
+                        if($user1['rank'] == 1)
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor mantelzorger gerapporteerd door client.",0,1);
+                        }
+                        else
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor mantelzorger gerapporteerd door mantelzorger.",0,1);
+                        }
+                    }
+                    else if($antwoordUser1['antwoord'] == 5)
+                    {
+                        if($user1['rank'] == 1)
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor beide gerapporteerd door client.",0,1);
+                        }
+                        else
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor beide gerapporteerd door mantelzorger.",0,1);
+                        }
+                    }
+
+                    if($antwoordUser2['antwoord'] == 3)
+                    {
+                        if($user2['rank'] == 1)
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor client gerapporteerd door client.",0,1);
+                        }
+                        else
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor client gerapporteerd door mantelzorger.",0,1);
+                        }
+                    }
+                    else if($antwoordUser2['antwoord'] == 4)
+                    {
+                        if($user2['rank'] == 1)
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor mantelzorger gerapporteerd door client.",0,1);
+                        }
+                        else
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor mantelzorger gerapporteerd door mantelzorger.",0,1);
+                        }
+                    }
+                    else if($antwoordUser2['antwoord'] == 5)
+                    {
+                        if($user2['rank'] == 1)
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor beide gerapporteerd door client.",0,1);
+                        }
+                        else
+                        {
+                            $pdf->Cell(10);
+                            $pdf->Cell(0,10,"- Hinder voor beide gerapporteerd door mantelzorger.",0,1);
+                        }
+                    }
+
+                    if($antwoordUser1['werk'] == 1 && $antwoordUser2['werk'] == 1)
+                    {
+                        $pdf->Cell(10);
+                        $pdf->SetFont('Times', 'B', 10);
+                        $pdf->Cell(0,10,"- Hulpvraag gesteld door beide.",0,1);
+                    }
+                    else if($antwoordUser1['werk'] == 1)
+                    {
+                        if($user1['rank'] == 1)
+                        {
+                            $pdf->Cell(10);
+                            $pdf->SetFont('Times', 'B', 10);
+                            $pdf->Cell(0,10,"- Hulpvraag gesteld door client.",0,1);
+                        }
+                        else
+                        {
+                            $pdf->Cell(10);
+                            $pdf->SetFont('Times', 'B', 10);
+                            $pdf->Cell(0,10,"- Hulpvraag gesteld door mantelzorger.",0,1);
+                        }
+                    }
+                    else if($antwoordUser2['werk'] == 1)
+                    {
+                        if($user2['rank'] == 1)
+                        {
+                            $pdf->Cell(10);
+                            $pdf->SetFont('Times', 'B', 10);
+                            $pdf->Cell(0,10,"- Hulpvraag gesteld door client.",0,1);
+                        }
+                        else
+                        {
+                            $pdf->Cell(10);
+                            $pdf->SetFont('Times', 'B', 10);
+                            $pdf->Cell(0,10,"- Hulpvraag gesteld door mantelzorger.",0,1);
+                        }
+                    }
+                }
+            } 
+            
+        }
+
+        
+              
+        $pdf->Output();
+    }
+    
+?>
